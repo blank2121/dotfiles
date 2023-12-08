0770ba92ea4cd58f5412a3a8d028f17c75582ec1
blank2121 <winstonwalter07@gmail.com>
2023-12-08 12:40:40 -0500

    added dots

--- .config/links.sh
+++ .config/links.sh
@@ -8,3 +8,24 @@ ln -s ~/dotfiles/.config/home-manager/home.nix ~/.config/home-manager/
 # Starship
 rm ~/.config/starship.toml
 ln -s ~/dotfiles/.config/starship.toml ~/.config/
+
+# Kitty
+rm -rf ~/.config/kitty/
+mkdir ~/.config/kitty/
+ln -s ~/dotfiles/.config/kitty/* ~/.config/kitty/
+
+# Waybar
+rm -rf ~/.config/waybar/
+mkdir ~/.config/waybar/
+ln -s ~/dotfiles/.config/waybar/* ~/.config/waybar/
+
+# hypr
+rm -rf ~/.config/hypr/
+mkdir ~/.config/hypr/
+ln -s ~/dotfiles/.config/hypr/* ~/.config/hypr/
+
+# zellij
+
+# lf
+
+# nvim
--- .config/nvim/copy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/.config/nvim/
-cp -r ~/dotfiles/.config/nvim/ ~/.config/
